61513313ee760e3d794402b2d9e31e7e3c221011
rbraband521 <57102562+rbraband521@users.noreply.github.com>
2020-08-03 13:51:28 -0500

    Update app.js
    Added comments

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-
+//Routes
 const routes = require('./routes/index');
 const books = require('./routes/books');
 
 const app = express();
 
-
-
 // view engine setup
-// app.use('/static', express.static('public'));
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'pug');
-
-// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -28,7 +23,7 @@ app.use( (req, res, next) => {
     next(createError(404));
   });
 
-  // error handler
+// error handler
   app.use( (err, req, res, next) => {
     if(err.status === 404) {
         console.log(err);
@@ -40,11 +35,9 @@ app.use( (req, res, next) => {
         res.render('error', {err, title: "Server Error"});
     }
   });
-
+//SERVER
 app.listen(3000, () => {
     console.log('The application is running on localhost: 3000')
-
 });
-
 
 module.exports = app;
